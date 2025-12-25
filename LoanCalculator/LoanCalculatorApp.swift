5496ee0186202fc7329cadc9323cbb043d57f0e6
Roman Moshenskiy <roman.moshenskiy@gemspace.com>
2025-12-25 18:37:40 +0200

    Initial Commit

---
+++ LoanCalculator/LoanCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanCalculatorApp.swift
+//  LoanCalculator
+//
+//  Created by Dev on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct LoanCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
